c2a5e0e5ea37570b7783367aa06fb81cc2917600
yugo333 <yugo199103@gmail.com>
2020-12-16 11:01:57 +0900

    moreJSX

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 
 function App() {
   return (
@@ -6,6 +6,20 @@ function App() {
       <h1>
         <code>src/App.js</code>マンマンママンマンマ
       </h1>
+      <div className='VVVV'>DIVDIV</div>
+      <input type="text" onClick={()=> {alert('まんままんま')}}/>
+      {/* 下記はdivタグがないとエラーになる。制約状一つに纏めないと動かない(labelはinputなどとセットで扱うため) */}
+    <div>
+    <label htmlFor="bar">bar</label>
+      <input type="text" onChange={()=> {alert('まんままんま')}}/>
+    </div>
+    {/* 上記では、一つに纏めないと出力できないのでdivを入れたが、divタグがブラウザーに表示されてしまう。それをしたくないときdivが嫌な時は下記 */}
+    {/* <React.Fragment> これを使うが<></>空白タグでも同じことが起きる*/}
+    <>
+    <label htmlFor="bar">bar</label>
+      <input type="text" onChange={()=> {alert('まんままんま')}}/>
+    </>
+    {/* </React.Fragment> */}
     </div>
   );
 }
