0a86fe7c1ec84f7bc8171dfcf6de774c41f57e1f
yugo333 <yugo199103@gmail.com>
2020-12-21 10:03:38 +0900

    types

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import { render } from "@testing-library/react";
+import PropTypes from 'prop-types'
 
 //関数コンポーネント
 
 const App=()=>{
   // ここで配列を指定しreturn内でmapで一つ一つ呼び出す
   const profiles=[
-    {name:"taro",age:"28"},
-    {name:"hana",age:"18"},
+    {name:"taro",age:28},
+    {name:"hana",age:18},
     {name:"NNNN"}
   ]
   return(
@@ -25,10 +26,17 @@ const App=()=>{
 const User =(props)=>{
   return <div>HI i'm {props.name},and {props.age}years old</div>
 }
-// default値を作ってあげる事で未設定でもレンダーできる
-User.defaultProps={
-  age:1
+// 型定義propTypesを使う
+User.propTypes={
+  name: PropTypes.string,
+  age: PropTypes.number
+  // .isRequired を末尾につけることにより必ずageの設定をしてなくてはならないってことになる
+  // age:PropTypes.number.isRequired
 }
+// default値を作ってあげる事で未設定でもレンダーできる
+// User.defaultProps={
+//   age:1
+// }
 
 
 export default App;
